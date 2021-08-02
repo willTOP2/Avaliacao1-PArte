13e2ab52986ef9198ecba27472b7ccdcf4a2b409
Willian <willian.rodrygues146@gmail.com>
2021-08-02 20:34:50 -0300

    Refatoração

--- ex2.js
+++ ex2.js
@@ -1,88 +1,88 @@
-const R = require('ramda');
+    const R = require('ramda');
 
- const isEven = (number) =>  {
-    const n = R.clone(number);
-    n.isEven = n.value % 2 == 0;
-    return n
-}
+    const isEven = (number) =>  {
+        const n = R.clone(number);
+        n.isEven = n.value % 2 == 0;
+        return n
+    }
 
- const positive = (number) => {
-    const n = R.clone(number);
-    n.positive = n.value > 0;
-    return n
-}
+    const positive = (number) => {
+        const n = R.clone(number);
+        n.positive = n.value > 0;
+        return n
+    }
 
- const isOdd = (number) => {
+    const isOdd = (number) => {
+        
+    const n = R.clone(number); 
+    n.isOdd = n.value % 2 !== 0; 
+    return n;
+
+    }
+
+    const negative = (number) => {
+        const n = R.clone(number); 
+        n.negative = n.value < 0; 
+        return n
+    }
+
+    const isZero = (number) =>  {
+        const n = R.clone(number); 
+        n.isZero = n.value === 0; 
+        return n;
+    }
+
+    const isPrime = (number) => {
+        const n = R.clone(number); 
+    n.isPrime = ValidPrime(number)
     
-  const n = R.clone(number); 
-  n.isOdd = n.value % 2 !== 0; 
-  return n;
+        return n;
+    }
 
-}
-
-const negative = (number) => {
-    const n = R.clone(number); 
-    n.negative = n.value < 0; 
-    return n
-}
-
-const isZero = (number) =>  {
-    const n = R.clone(number); 
-    n.isZero = n.value === 0; 
-    return n;
-}
-
-const isPrime = (number) => {
-    const n = R.clone(number); 
-   n.isPrime = ValidPrime(number)
-   
-    return n;
-}
-
-const ValidPrime = (num) => {
-    for (let i = 2; i < num; i++)
-      if (num % i === 0) {
-        return false;
-      }
-    return num > 1;
-  };
+    const ValidPrime = (num) => {
+        for (let i = 2; i < num; i++)
+        if (num % i === 0) {
+            return false;
+        }
+        return num > 1;
+    };
 
 
 
-const mapToNumberObject = num => {
-    return { value: num };
-}
+    const mapToNumberObject = num => {
+        return { value: num };
+    }
 
-const arr = [-1, 50, 5, 10, -8, 20, 25, 0, 100, 14, -123]; 
-
-
-  const map = arr.map(mapToNumberObject)
+    const arr = [-1, 50, 5, 10, -8, 20, 25, 0, 100, 14, -123]; 
 
 
+    const map = arr.map(mapToNumberObject)
 
- const compose = R.pipe( 
-     
-     isEven, 
-     isZero, 
-     negative, 
-     positive, 
-     isPrime, 
-     isOdd
-     
- )  
 
-  
- 
 
-module.exports = {isEven, isOdd, isPrime,isZero, positive, negative, compose, mapToNumberObject}
+    const compose = R.pipe( 
+        
+        isEven, 
+        isZero, 
+        negative, 
+        positive, 
+        isPrime, 
+        isOdd
+        
+    )  
 
-// ExercÃ­cio 1: use map() para transformar 'arr' em objetos usando mapToNumberObject()
+    
+    
 
-// ExercÃ­cio 2: seguindo o modelo das 2 primeiras funÃ§Ãµes implemente as outras 4 funÃ§Ãµes
+    module.exports = {isEven, isOdd, isPrime,isZero, positive, negative, compose, mapToNumberObject}
 
-// ExercÃ­cio 3: refatore todas as funÃ§Ãµes para a forma usando arrow function (=>)
+    // ExercÃ­cio 1: use map() para transformar 'arr' em objetos usando mapToNumberObject()
 
-// ExercÃ­cio 4: use R.pipe para compor as funÃ§Ãµes: isEven, positive, isOdd, negative, 
-    // isZero, e isPrime. Teste a funÃ§Ã£o composta com um Ãºnico objeto.
+    // ExercÃ­cio 2: seguindo o modelo das 2 primeiras funÃ§Ãµes implemente as outras 4 funÃ§Ãµes
 
-// ExercÃ­cio 5: use a funÃ§Ã£o composta do Ex. 4 para transformar os nÃºmeros em 'arr'
+    // ExercÃ­cio 3: refatore todas as funÃ§Ãµes para a forma usando arrow function (=>)
+
+    // ExercÃ­cio 4: use R.pipe para compor as funÃ§Ãµes: isEven, positive, isOdd, negative, 
+        // isZero, e isPrime. Teste a funÃ§Ã£o composta com um Ãºnico objeto.
+
+    // ExercÃ­cio 5: use a funÃ§Ã£o composta do Ex. 4 para transformar os nÃºmeros em 'arr'
--- testes.test.js
+++ testes.test.js
@@ -1,63 +1,63 @@
 
 
-const Sistema = require('./ex2')
+  const Sistema = require('./ex2')
 
 
- describe('Testes', ()=> {
-    
-const arr = [-1, 50, 5]; 
- const result = Sistema.compose({value: 0})
+  describe('Testes', ()=> {
+      
+  const arr = [-1, 50, 5]; 
+  const result = Sistema.compose({value: 0})
 
- const result1 = arr.map(Sistema.mapToNumberObject).map(Sistema.compose) 
+  const result1 = arr.map(Sistema.mapToNumberObject).map(Sistema.compose) 
 
-    test('Teste Exercicio 4', ()=> {
+      test('Teste Exercicio 4', ()=> {
 
-        expect(result).toStrictEqual({
-          value: 0,
-          isEven: true,
-          isZero: true,
-          negative: false,
-          positive: false,
-          isPrime: false,
-          isOdd: false
-        })
+          expect(result).toStrictEqual({
+            value: 0,
+            isEven: true,
+            isZero: true,
+            negative: false,
+            positive: false,
+            isPrime: false,
+            isOdd: false
+          })
 
+      })
+
+      test('Teste Exercicio 5', ()=> { 
+        expect(result1).toStrictEqual(
+        [
+          {
+            value: -1,
+            isEven: false,
+            isZero: false,
+            negative: true,
+            positive: false,
+            isPrime: false,
+            isOdd: true
+          },
+          {
+            value: 50,
+            isEven: true,
+            isZero: false,
+            negative: false,
+            positive: true,
+            isPrime: false,
+            isOdd: false
+          },
+          {
+            value: 5,
+            isEven: false,
+            isZero: false,
+            negative: false,
+            positive: true,
+            isPrime: false,
+            isOdd: true
+          }
+        ]
+        )
+      
     })
+  
 
-    test('Teste Exercicio 5', ()=> { 
-      expect(result1).toStrictEqual(
-      [
-        {
-          value: -1,
-          isEven: false,
-          isZero: false,
-          negative: true,
-          positive: false,
-          isPrime: false,
-          isOdd: true
-        },
-        {
-          value: 50,
-          isEven: true,
-          isZero: false,
-          negative: false,
-          positive: true,
-          isPrime: false,
-          isOdd: false
-        },
-        {
-          value: 5,
-          isEven: false,
-          isZero: false,
-          negative: false,
-          positive: true,
-          isPrime: false,
-          isOdd: true
-        }
-      ]
-      )
-    
   })
- 
-
-})
